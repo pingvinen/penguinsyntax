823d491ac9543e732e878e452e8fb07cfbd983f2
Patrick <pingvinen@isflagen.dk>
2013-03-11 14:06:30 +0100

    Added an overload of the IsPatternPresent method that takes an IList<T> as the pattern.

--- src/lib/Extensions/IListExtensions.cs
+++ src/lib/Extensions/IListExtensions.cs
@@ -32,10 +32,10 @@ namespace PenguinSyntax.Extensions
 		/// <typeparam name="T">
 		/// The list element type
 		/// </typeparam>
-		public static bool IsPatternPresent<T>(this IList<T> list, int startIndex, Func<T, T, bool> tester, params T[] pattern)
+		public static bool IsPatternPresent<T>(this IList<T> list, int startIndex, Func<T, T, bool> tester, IList<T> pattern)
 		{
 			int listIndex;
-			for (int patternIndex=0; patternIndex!=pattern.Length; patternIndex++)
+			for (int patternIndex=0; patternIndex!=pattern.Count; patternIndex++)
 			{
 				// calculate the index in the list
 				listIndex = startIndex + patternIndex;
@@ -54,6 +54,34 @@ namespace PenguinSyntax.Extensions
 			}
 
 			return true;
+		}
+
+		/// <summary>
+		/// Check whether a given pattern is present in the given list from the given index
+		/// </summary>
+		/// <returns>
+		/// <c>true</c> if the pattern is present; <c>false</c> otherwise
+		/// </returns>
+		/// <param name="list">
+		/// The list to check
+		/// </param>
+		/// <param name="startIndex">
+		/// The index in <paramref name="list"/> to start with
+		/// </param>
+		/// <param name="tester">
+		/// A method that takes the current element in <paramref name="list"/>
+		/// and the current element in <paramref name="pattern"/> and returns a boolean
+		/// stating whether they match
+		/// </param>
+		/// <param name="pattern">
+		/// The set of elements to look for
+		/// </param>
+		/// <typeparam name="T">
+		/// The list element type
+		/// </typeparam>
+		public static bool IsPatternPresent<T>(this IList<T> list, int startIndex, Func<T, T, bool> tester, params T[] pattern)
+		{
+			return list.IsPatternPresent(startIndex, tester, new List<T>(pattern));
 		}
 		#endregion Is pattern present
 	}
